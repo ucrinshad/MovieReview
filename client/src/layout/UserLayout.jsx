04b218c6700d16c1d0d35086c3dad8328dcf8693
rinshad <rinshad>
2024-12-19 00:03:20 +0530

    check-user

--- client/src/layout/UserLayout.jsx
+++ client/src/layout/UserLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Header } from "../components/user/Header";
 import { Footer } from "../components/user/Footer";
 import { Outlet, useLocation } from "react-router-dom";
@@ -8,49 +8,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { clearUserData, saveUserData } from "../redux/features/userSlice";
 
 export const UserLayout = () => {
-
-    const {isUserAuth,userData} = useSelector((state)=>state.user);
+    const { isUserAuth, userData } = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const location = useLocation();
-    
-    
 
-
-    const checkUser = async()=> {
+    const checkUser = async () => {
         try {
             const response = await axiosIstance({
-                method:"GET",
-                url:"/user/check-user"
-            })
+                method: "GET",
+                url: "/user/check-user",
+                withCredentials: true,  // Ensure credentials (cookies) are sent
+            });
 
-            dispatch(saveUserData());
+            dispatch(saveUserData(response.data));  // Save user data to Redux store
             console.log(response, '=====checkuser response');
-            
         } catch (error) {
             dispatch(clearUserData());
-            console.log(error);
             console.log(error, '=====checkuser error response');
         }
-    }
+    };
 
-    console.log(isUserAuth, 'isUserAuth');
-    
-    console.log(userData, 'userData');
-    
-
-    useEffect(() =>{
-        checkUser()
-    },[location.pathname])
-
+    // Trigger checkUser only once when user is not authenticated
+    useEffect(() => {
+        if (!isUserAuth) {
+            checkUser();
+        }
+    }, [isUserAuth]);  // Dependency on isUserAuth
 
     return (
         <div>
-            {isUserAuth? <UserHeader/> : <Header />
-        }
-            
-            <div className="min-h-96"><Outlet/></div>
-            
+            {isUserAuth ? <UserHeader /> : <Header />}
+            <div className="min-h-96">
+                <Outlet />
+            </div>
             <Footer />
         </div>
-    )
+    );
 };
