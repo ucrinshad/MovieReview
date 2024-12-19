c3714b248ba327e3365728e99510fdcf4e8db36f
rinshad <rinshad>
2024-12-19 19:17:03 +0530

    cors jst

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-      origin: ["http://localhost:5173","https://moviereview-clientnew.vercel.app"] ,
-      credentials: true,
-      methods: ["GET","POST","PUT","DELETE"]
+    origin: ["http://localhost:5173", "https://moviereview-clientnew.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 
 app.use(cookieParser())
 connectDB()
