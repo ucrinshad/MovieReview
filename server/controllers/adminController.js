709ed6dceb2bc79b4e318b7f271ed88f5d1c11ee
rinshad <rinshad>
2024-12-19 20:11:45 +0530

    jst

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,137 +1,107 @@
 
-import bcrypt from "bcrypt"
+import { User } from "../models/userModel.js";
+import bcrypt from "bcrypt";
 import { generateToken } from "../utils/token.js";
 import { Admin } from "../models/adminModel.js";
 
 
-
-export const adminSignup = async(req,res,next) =>{
+export const adminSignup = async (req, res, next) => {
     try {
-        const {name,email,password,profilePic}= req.body;
+        const { name, email, password,  profilePic } = req.body;
 
-        if(!name ||!email ||!password ){
-            return res.status(400).json({message:"all fields required"})
+        if (!name || !email || !password ) {
+            return res.status(400).json({ message: "all fields required" });
         }
-        const userExist = await Admin.findOne({email:email})
-        if(userExist){
-            return res.status(400).json({message:"admin already exist"})
 
+        const userExist = await Admin.findOne({ email: email });
+
+        if (userExist) {
+            return res.status(400).json({ message: "mentor already exist" });
         }
 
         const hashedPassword = bcrypt.hashSync(password, 10);
 
-        console.log('password====', password);
-        console.log('hashedPassword====', hashedPassword)
-
-
-        const newUser = new Admin({name,email,password:hashedPassword,profilePic})
+        const newUser = new Admin({ name, email, password: hashedPassword, mobile, profilePic });
         await newUser.save();
 
-
-        const token= generateToken(newUser,'admin')
-
-        res.cookie("token", token,{
-            sameSite:"None",
-            secure:true,
-            httpOnly:true,
+        const token = generateToken(newUser, "admin");
+        res.cookie("token", token, {
+            sameSite: "None",
+            secure: true,
+            httpOnly: true,
         });
 
-
-        res.json({message:'admin created successfully'})
-
-        
+        res.json({ message: "admin created successfully" });
     } catch (error) {
-        res.status(error.statuscode || 500).json({message:error.message || 'Internal server error'})
+        res.status(error.statusCode || 500).json({ message: error.message || "Internal server error" });
     }
-}
+};
 
-export const adminLogin = async(req,res,next) =>{
+export const adminLogin = async (req, res, next) => {
     try {
-        const {email,password}= req.body;
+        const { email, password } = req.body;
 
-        if(!email ||!password ){
-            return res.status(400).json({message:"all fields required"})
-        }
-        const userExist = await Admin.findOne({email:email})
-
-        if(!userExist){
-            return res.status(400).json({message:"admin does not exist"})
-
+        if (!email || !password) {
+            return res.status(400).json({ message: "all fields required" });
         }
 
-        const isPasswordMatch = bcrypt.compareSync(password, userExist.password)
+        const userExist = await Admin.findOne({ email: email });
 
-        if(!isPasswordMatch){
-            return res.status(400).json({message: 'admin not authenticated'})
+        if (!userExist) {
+            return res.status(400).json({ message: "admin does not exist" });
         }
 
-        
+        const isPasswordMatch = bcrypt.compareSync(password, userExist.password);
 
-        
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: "admin not authenticated" });
+        }
 
-
-
-        const token= generateToken(userExist,'admin')
-        console.log('token generated====',token)
-
-        res.cookie("token", token,{
-            sameSite:"None",
-            secure:true,
-            httpOnly:true,
+        const token = generateToken(userExist, "admin");
+        res.cookie("token", token, {
+            sameSite: "None",
+            secure: true,
+            httpOnly: true,
         });
 
-
-        res.json({message:'admin login successfully'})
-
-        
+        res.json({ message: "admin login successfully" });
     } catch (error) {
-        res.status(error.statuscode || 500).json({message:error.message || 'Internal server error'})
+        res.status(error.statusCode || 500).json({ message: error.message || "Internal server error" });
     }
-}
+};
 
-export const adminProfile = async(req,res,next) =>{
+export const adminProfile = async (req, res, next) => {
     try {
         const userId = req.user.id;
+        const userProfile = await Admin.findById(userId).select("-password");
 
-        const userProfile = await Admin.findById(userId).select("-password")
-
-
-        res.json({message:'admin login successfully',data:userProfile})
-
-        
+        res.json({ message: "user login successfully", data: userProfile });
     } catch (error) {
-        res.status(error.statuscode || 500).json({message:error.message || 'Internal server error'})
+        res.status(error.statusCode || 500).json({ message: error.message || "Internal server error" });
     }
-}
+};
 
-export const adminLogout = async(req,res,next) =>{
+export const adminLogout = async (req, res, next) => {
     try {
-        res.clearCookie("token" ,{
-            sameSite:"None",
-            secure:true,
-            httpOnly:true,
+        res.clearCookie("token", {
+            sameSite: "None",
+            secure: true,
+            httpOnly: true,
         });
 
-        res.json({message: "admin logout successfully", data:adminProfile})
-
-
+        res.json({ message: "admin logout success" });
     } catch (error) {
-        res.status(error.statuscode || 500).json({message:error.message || 'Internal server error'})
+        res.status(error.statusCode || 500).json({ message: error.message || "Internal server error" });
     }
-}
+};
 
-export const checkAdmin = async(req,res,next) =>{
+export const checkAdmin = async (req, res, next) => {
     try {
-        
-
-        res.json({message: "admin authorised"})
-
-
+        res.json({ message: "admin autherized" });
     } catch (error) {
-        res.status(error.statuscode || 500).json({message:error.message || 'Internal server error'})
+        res.status(error.statusCode || 500).json({ message: error.message || "Internal server error" });
     }
-}
-
+};
 export const updateAdmin = async(req, res, next)=>{
 
     try {
@@ -139,7 +109,7 @@ export const updateAdmin = async(req, res, next)=>{
 
         const updatedAdmin = await Admin.findByIdAndUpdate(req.params.id, req.body,{ new: true});
         
-        if(updateAdmin){
+        if(updatedAdmin){
             res.status(200).json(updatedAdmin);
         }else{
             res.status(404).json({ message: " Update not found "})
@@ -148,16 +118,3 @@ export const updateAdmin = async(req, res, next)=>{
         res.status(500).json({message: error.message})
     }
 } 
-export const deleteAdmin = async(req,res,next)=>{
-
-    try {
-        const deletedAdmin = await Admin.findByIdAndDelete(req.params.id);
-
-        if(deletedAdmin) {
-            res.status(200).json({ message: "asmin deleted successfully"})
-            res.status(404).json({message: "admin not found"})
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
